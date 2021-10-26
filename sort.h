6899d1c6e506c60d7e8c9437cfb53a9b5d9663be
Tihitna <tihitnasete58@gmail.com>
2021-10-26 12:27:35 -0700

    bubble

--- sort.h
+++ sort.h
@@ -1,19 +1,31 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdlib.h>
+/*includes*/
 #include <stdio.h>
-#include <stddef.h>
-#include <stdbool.h>
-void swap(int *x, int *y);
+#include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* enum bool - Enumeration of Boolean values.
+* @false: Equals 0.
+* @true: Equals 1.
+*/
+typedef enum bool
+{
+false = 0,
+true
+} bool;
+/**
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
 const int n;
@@ -21,9 +33,11 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
+/*Given printing functions*/
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
+/*Sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -37,13 +51,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-int swap_nodes(listint_t *list);
-
-void quickSort(int *arr, size_t size, int low, int high);
-int partition(int *arr, size_t size, int low, int high);
-
-size_t knuth_idx(size_t size);
-size_t gapf(size_t idx);
-size_t pow1(size_t base, size_t exp);
-
-#endif /* HOLBERTON_H */
+#endif

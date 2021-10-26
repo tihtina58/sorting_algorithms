5b9db4a954ab0221faff3dd18be70e447398975c
Tihitna <tihitnasete58@gmail.com>
2021-10-26 12:15:22 -0700

    bubble' quit exit z

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,35 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using the
- * Bubble sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: Array to sort
+ * @size: size of the array
  *
- * Return: void
+ * Return: Void
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, n, new_n;
-int swap;
+size_t i, j, size2 = size;
+int tmp, flag = 0;
 if (array == NULL || size < 2)
 return;
-n = size;
-while (n > 0)
+for (i = 0; i < size; i++)
 {
-new_n = 0;
-for (i = 0; i < n - 1; i++)
+for (j = 1; j < size2; j++)
 {
-if (array[i] > array[i + 1])
+if (array[j - 1] > array[j])
 {
-swap = array[i];
-array[i] = array[i + 1];
-array[i + 1] = swap;
-new_n = i + 1;
+flag = 1;
+tmp = array[j];
+array[j] = array[j - 1];
+array[j - 1] = tmp;
 print_array(array, size);
 }
 }
-n = new_n;
+if (flag == 0)
+break;
+flag = 0;
+size2--;
 }
 }

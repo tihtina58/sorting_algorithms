cf93747346670f7464d993dcf2e4fdca687a3a5d
Tihitna <tihitnasete58@gmail.com>
2021-10-26 22:37:08 -0700

    bubble

--- sort.h
+++ sort.h
@@ -1,24 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
-/*includes*/
-#include <stdio.h>
 #include <stdlib.h>
-
-/* Comparison direction macros for bitonic sort */
-#define UP 0
-#define DOWN 1
-
-/**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum bool
-{
-false = 0,
-true
-} bool;
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -34,11 +18,9 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-/*Given printing functions*/
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
-/*Sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -52,4 +34,13 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif
+int swap_nodes(listint_t *list);
+
+void quickSort(int *arr, size_t size, int low, int high);
+int partition(int *arr, size_t size, int low, int high);
+
+size_t knuth_idx(size_t size);
+size_t gapf(size_t idx);
+size_t pow1(size_t base, size_t exp);
+
+#endif /* SORT_H */
